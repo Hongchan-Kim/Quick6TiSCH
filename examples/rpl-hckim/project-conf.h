56d07c37385afebbd5e8d978d1fdc3037f937d14
hckim <hckim@netlab.snu.ac.kr>
2020-09-08 21:47:44 +0900

    MODIFY: enable logging in examples/rpl-hckim/project-conf.h

--- examples/rpl-hckim/project-conf.h
+++ examples/rpl-hckim/project-conf.h
@@ -1,4 +1,11 @@
 #ifndef PROJECT_CONF_H_
 #define PROJECT_CONF_H_
 
+#define LOG_CONF_LEVEL_IPV6                        LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_RPL                         LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_6LOWPAN                     LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_TCPIP                       LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_FRAMER                      LOG_LEVEL_INFO
+
 #endif /* PROJECT_CONF_H_ */ 
