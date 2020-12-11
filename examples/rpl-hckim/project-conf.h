060bc32886b1168ee3918a129eef10514c2b5ad8
Hongchan-Kim <hckim@netlab.snu.ac.kr>
2020-12-11 19:27:39 +0900

    MODIFY: improve project-conf.h

--- examples/rpl-hckim/project-conf.h
+++ examples/rpl-hckim/project-conf.h
@@ -8,4 +8,16 @@
 #define LOG_CONF_LEVEL_MAC                         LOG_LEVEL_INFO
 #define LOG_CONF_LEVEL_FRAMER                      LOG_LEVEL_INFO
 
+#define DOWNWARD_TRAFFIC                           1
+#define START_DELAY                                (1 * 60 * CLOCK_SECOND)
+#define SEND_INTERVAL                              (1 * 60 * CLOCK_SECOND)
+
+#define SIMPLE_ENERGEST_CONF_PERIOD                (CLOCK_SECOND * 60)
+
+/* m17dBm, m12dBm, m9dBm, m7dBm, m5dBm, m4dBm, m3dBm, m2dBm, m1dBm, 
+   0dBm, 0_7dBm, 1_3dBm, 1_8dBm, 2_3dBm, 2_8dBm, 3dBm, 0dBm */
+#define RF2XX_TX_POWER                             PHY_POWER_m17dBm
+/* m101dBm, m90dBm, m87dBm, m84dBm, m81dBm, m78dBm, m75dBm, m72dBm, 
+   m69dBm, m66dBm, m63dBm, m60dBm, m57dBm, m54dBm, m51dBm, m48dBm */
+#define RF2XX_RX_RSSI_THRESHOLD                    RF2XX_PHY_RX_THRESHOLD__m87dBm
 #endif /* PROJECT_CONF_H_ */ 
