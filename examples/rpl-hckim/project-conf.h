9eb3c5401f0d683ea6ca44bbd3e3cde7aae6e70d
Hongchan-Kim <hckim@netlab.snu.ac.kr>
2022-06-07 10:30:50 +0900

    Add new grenoble topology and set evaluation

--- examples/rpl-hckim/project-conf.h
+++ examples/rpl-hckim/project-conf.h
@@ -6,17 +6,10 @@
  * Exclusive period implementation
  */
 /* Need to be tested */
-#define MODIFIED_MAC_SEQNO_DUPLICATE_CHECK         1
-#define GET_NODE_ID_FROM_IPADDR(addr)              ((((addr)->u8[14]) << 8) | (addr)->u8[15])
-#define GET_NODE_ID_FROM_LINKADDR(addr)            ((((addr)->u8[LINKADDR_SIZE - 2]) << 8) | (addr)->u8[LINKADDR_SIZE - 1]) 
-
-#define WITH_TSCH_TX_CCA                           1
-#define WITH_PPSD                                  1
 #define WITH_TSCH_DEFAULT_BURST_TRANSMISSION       0
+#define WITH_PPSD                                  0
 
 #if WITH_PPSD
-#define EVAL_CONTROL_NUM_OF_PKTS_IN_EP             0 /* Needs WITH_PPSD */
-
 #define PPSD_HEADER_IE_IN_DATA_AND_ACK             1 /* Must be 1 if WITH_PPSD is 1*/
 #define PPSD_EP_POLICY_CELL_UTIL                   1
 #define PPSD_TX_SLOT_FORWARD_OFFLOADING            1
@@ -28,6 +21,8 @@
 #define PPSD_TEMPORAL_LINK                         1 /* To prevent current_link for EP from becomming NULL and skipped */
 #define PPSD_HANDLE_SKIPPED_EP_SLOT                1 /* To reset EP flags and stop EP in the case of skipped slot */
 #define PPSD_HANDLE_MISSED_EP_SLOT                 1 /* To reset EP flags and stop EP in the case of dl-miss main */
+
+#define EVAL_CONTROL_NUM_OF_PKTS_IN_EP             0 /* Needs WITH_PPSD */
 
 #define PPSD_EP_EXTRA_CHANNELS                     0 /* Extra channel hopping */
 #define PPSD_USE_BUSYWAIT                          0
@@ -49,6 +44,7 @@
 #define TSCH_PPSD_HOPPING_SEQUENCE_4_4 (uint8_t[]){ 11, 16, 19, 21 }
 #endif
 
+#define WITH_TSCH_TX_CCA                           1
 #if WITH_TSCH_TX_CCA
 #define TSCH_CONF_CCA_ENABLED                      1
 #define TSCH_TX_CCA_DBG_CCA_RESULT                 0
@@ -72,6 +68,9 @@
 #define TSCH_CONF_BURST_MAX_LEN                    0 /* turn burst off */
 #endif
 
+#define MODIFIED_MAC_SEQNO_DUPLICATE_CHECK         1
+#define GET_NODE_ID_FROM_IPADDR(addr)              ((((addr)->u8[14]) << 8) | (addr)->u8[15])
+#define GET_NODE_ID_FROM_LINKADDR(addr)            ((((addr)->u8[LINKADDR_SIZE - 2]) << 8) | (addr)->u8[LINKADDR_SIZE - 1]) 
 //#define PPSD_CONF_RX_WAIT                    800
 /*---------------------------------------------------------------------------*/
 
@@ -91,22 +90,26 @@
 #elif WITH_IOTLAB
 
 #define IOTLAB_GRENOBLE_83_R_CORNER                1 /* 83 nodes */
-#define IOTLAB_GRENOBLE_79_R_CORNER                2 /* 79 nodes */
-#define IOTLAB_GRENOBLE_79_L_CORNER                3 /* 79 nodes */
-#define IOTLAB_LILLE_79_CORNER                     4 /* 79 nodes */
-#define IOTLAB_LILLE_79_CENTER                     5 /* 79 nodes */
-#define IOTLAB_LYON_2                              6 /* 2 nodes */
-#define IOTLAB_LYON_3                              7 /* 3 nodes */
-#define IOTLAB_LYON_5                              8 /* 2 nodes */
-#define IOTLAB_LYON_8                              9 /* 8 nodes */
-#define IOTLAB_LYON_17                             10 /* 17 nodes */
+#define IOTLAB_GRENOBLE_79_R_CORNER_U              2 /* 79 nodes */
+#define IOTLAB_GRENOBLE_79_R_CORNER_D              3 /* 79 nodes */
+#define IOTLAB_GRENOBLE_79_L_CORNER_U              4 /* 79 nodes */
+#define IOTLAB_GRENOBLE_79_L_CORNER_D              5 /* 79 nodes */
+#define IOTLAB_LILLE_79_CORNER                     6 /* 79 nodes */
+#define IOTLAB_LILLE_79_CENTER                     7 /* 79 nodes */
+#define IOTLAB_LYON_2                              8 /* 2 nodes */
+#define IOTLAB_LYON_3                              9 /* 3 nodes */
+#define IOTLAB_LYON_5                              10 /* 2 nodes */
+#define IOTLAB_LYON_8                              11 /* 8 nodes */
+#define IOTLAB_LYON_17                             12 /* 17 nodes */
 
 //#define IOTLAB_SITE                                IOTLAB_GRENOBLE_83_R_CORNER
-//#define IOTLAB_SITE                                IOTLAB_GRENOBLE_79_R_CORNER
-//#define IOTLAB_SITE                                IOTLAB_GRENOBLE_79_L_CORNER
+//#define IOTLAB_SITE                                IOTLAB_GRENOBLE_79_R_CORNER_U
+#define IOTLAB_SITE                                IOTLAB_GRENOBLE_79_R_CORNER_D
+//#define IOTLAB_SITE                                IOTLAB_GRENOBLE_79_L_CORNER_U
+//#define IOTLAB_SITE                                IOTLAB_GRENOBLE_79_L_CORNER_D
 //#define IOTLAB_SITE                                IOTLAB_LILLE_79_CORNER
 //#define IOTLAB_SITE                                IOTLAB_LILLE_79_CENTER
-#define IOTLAB_SITE                                IOTLAB_LYON_2
+//#define IOTLAB_SITE                                IOTLAB_LYON_2
 //#define IOTLAB_SITE                                IOTLAB_LYON_3
 //#define IOTLAB_SITE                                IOTLAB_LYON_5
 //#define IOTLAB_SITE                                IOTLAB_LYON_8
@@ -114,9 +117,13 @@
 
 #if IOTLAB_SITE == IOTLAB_GRENOBLE_83_R_CORNER
 #define NODE_NUM                                   83
-#elif IOTLAB_SITE == IOTLAB_GRENOBLE_79_R_CORNER
+#elif IOTLAB_SITE == IOTLAB_GRENOBLE_79_R_CORNER_U
 #define NODE_NUM                                   79
-#elif IOTLAB_SITE == IOTLAB_GRENOBLE_79_L_CORNER
+#elif IOTLAB_SITE == IOTLAB_GRENOBLE_79_R_CORNER_D
+#define NODE_NUM                                   79
+#elif IOTLAB_SITE == IOTLAB_GRENOBLE_79_L_CORNER_U
+#define NODE_NUM                                   79
+#elif IOTLAB_SITE == IOTLAB_GRENOBLE_79_L_CORNER_D
 #define NODE_NUM                                   79
 #elif IOTLAB_SITE == IOTLAB_LILLE_79_CORNER
 #define NODE_NUM                                   79
@@ -180,11 +187,11 @@
 #define APP_PRINT_DELAY                            (1 * 60 * CLOCK_SECOND / 2)
 
 #elif WITH_IOTLAB
-#define APP_UPWARD_SEND_INTERVAL                   (1 * 60 * CLOCK_SECOND / 60)
+#define APP_UPWARD_SEND_INTERVAL                   (1 * 60 * CLOCK_SECOND / 6)
 #define DOWNWARD_TRAFFIC                           0
 #define APP_DOWNWARD_SEND_INTERVAL                 (1 * 60 * CLOCK_SECOND / 60)
-#define APP_START_DELAY                            (2 * 60 * CLOCK_SECOND)
-#define APP_DATA_PERIOD                            (8 * 60 * CLOCK_SECOND)
+#define APP_START_DELAY                            (60 * 60 * CLOCK_SECOND)
+#define APP_DATA_PERIOD                            (60 * 60 * CLOCK_SECOND)
 #define APP_PRINT_DELAY                            (1 * 60 * CLOCK_SECOND)
 #endif
 
@@ -196,7 +203,13 @@
 #define VARY_LENGTH                                8
 #endif
 
-#define APP_PAYLOAD_LEN                            80
+#define APP_PAYLOAD_LEN                            14 // Min len with App footer
+//#define APP_PAYLOAD_LEN                            86 // Max len of Orchestra/ALICE in single hop
+//#define APP_PAYLOAD_LEN                            80 // Max len of Orchestra/ALICE + EP in single hop
+//#define APP_PAYLOAD_LEN                            84 // Max len of OST w/o ODP in single hop
+//#define APP_PAYLOAD_LEN                            82 // Max len of OST in single hop
+//#define APP_PAYLOAD_LEN                            78 // Max len of OST + EP in single hop
+
 #define APP_DATA_MAGIC                             0x58FE
 
 #define APP_SEQNO_DUPLICATE_CHECK                  1
@@ -272,8 +285,8 @@
 
 //#define CURRENT_TSCH_SCHEDULER                     TSCH_SCHEDULER_NB_ORCHESTRA
 //#define CURRENT_TSCH_SCHEDULER                     TSCH_SCHEDULER_LB_ORCHESTRA
-//#define CURRENT_TSCH_SCHEDULER                     TSCH_SCHEDULER_ALICE
-#define CURRENT_TSCH_SCHEDULER                     TSCH_SCHEDULER_OST
+#define CURRENT_TSCH_SCHEDULER                     TSCH_SCHEDULER_ALICE
+//#define CURRENT_TSCH_SCHEDULER                     TSCH_SCHEDULER_OST
 
 #define ORCHESTRA_RULE_NB { &eb_per_time_source, \
                           &unicast_per_neighbor_rpl_storing, \
@@ -321,7 +334,7 @@
 #define ORCHESTRA_CONF_UNICAST_SENDER_BASED        1 //1: sender-based, 0:receiver-based
 #define ORCHESTRA_CONF_EBSF_PERIOD                 397 // EB, original: 397
 #define ORCHESTRA_CONF_COMMON_SHARED_PERIOD        19 // broadcast and default slotframe length, original: 31
-#define ORCHESTRA_CONF_UNICAST_PERIOD              311//23 // unicast, should be longer than (2N-2)/3 to provide contention-free links
+#define ORCHESTRA_CONF_UNICAST_PERIOD              17 // unicast, should be longer than (2N-2)/3 to provide contention-free links
 
 #define ALICE_PACKET_CELL_MATCHING_ON_THE_FLY      alice_packet_cell_matching_on_the_fly
 #define ALICE_TIME_VARYING_SCHEDULING              alice_time_varying_scheduling
@@ -344,7 +357,7 @@
 #define ORCHESTRA_CONF_UNICAST_PERIOD              47 // unicast, 7, 11, 23, 31, 43, 47, 59, 67, 71    
 #define ORCHESTRA_CONF_COMMON_SHARED_PERIOD        41 //31 broadcast and default slotframe length, original: 31
 
-#define OST_ON_DEMAND_PROVISION                    1
+#define OST_ON_DEMAND_PROVISION                    0
 
 #define OST_HANDLE_QUEUED_PACKETS                  1
 #define WITH_OST_LOG_INFO                          1
